3c1391b4f82c7eb3039e7751d9272af9fb940d31
Rudra <rudravamukhejee.2001@gmail.com>
2021-03-19 15:20:06 +0530

    day 2 module 3 exercise complete S-commit

--- App.js
+++ App.js
@@ -3,54 +3,16 @@ import React from "react";
 import {
   View,
 } from "react-native";
+import ViewImageScreens from "./app/screens/ViewImageScreens";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-      <View
-        style={{
-          backgroundColor: "#ffc",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center", //main axis allingment
-          // alignContent: "center", //this centers a multiline content as a whole 
-          // flexWrap: "wrap",
-          // on multiline align items behaves as alginent per each line 
-          alignItems: "center", //along secondary axis
-      }}>
-        <View style={{
-          backgroundColor: "dodgerblue",
-          // flexGrow: 1,
-          // flexBasis: 100,
-          // width: 300,
-          // flexShrink: 1,
-          width: 100,
-          height: 100,
-          // alignSelf: "flex-start"
-        }} />
-        <View style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          position: "absolute",
-          top: 30,
-           left: 20
-        }} />
-        <View style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100
-        }} />
-        {/* <View style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100
-        }} />
-        <View style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100
-        }} /> */}
-      </View>
+    <>
+    <WelcomeScreen/>
+    {/* <ViewImageScreens/> */}
+    </>
+    
   );
 }
 
---
+++ app/config/colors.js
@@ -0,0 +1,5 @@
+export default {
+  primary: "#fc5c65",
+  secondary: "#4ecdcf",
+  black: "#1d1d1d",
+};
